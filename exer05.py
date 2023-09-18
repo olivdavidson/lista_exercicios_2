232046c27d70f14ef947a5adc9a72be456e7f9aa
oldavidson <olidavidson.str@gmail.com>
2023-09-18 18:49:41 -0400

    Atualizando lista de Exercícios

---
+++ exer05.py
@@ -0,0 +1,5 @@
+num1 = int(input('Informe um número: '))
+if (num1 % 2 == 0):
+    print('Número par!')
+else:
+    print('Número ímpar!')
---
+++ exer06.py
@@ -0,0 +1,8 @@
+cont = 0
+lista_num = []
+while cont != 2:
+    numero = int(input('Informe um número: '))
+    lista_num.append(numero)
+    cont = cont + 1
+#soma = sum(lista_num)
+print('O maior número informado é ',max(lista_num),' e a diferença entre os números é de ')
